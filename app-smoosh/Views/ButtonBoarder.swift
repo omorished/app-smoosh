06a451b40de53fde6b6c2c036431189577ee5d6e
osamah morished <omorished@gmail.com>
2019-05-17 07:25:44 +0300

    Start building app and place Images, labels and button to the welcome screen

---
+++ app-smoosh/Views/ButtonBoarder.swift
@@ -0,0 +1,14 @@
+
+
+import UIKit
+
+class ButtonBoarder: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+
+}
